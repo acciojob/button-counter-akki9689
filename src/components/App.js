e32beb0c238c8ec2b64649b8b0627508d68f8aa6
Akash Puri <104100720+akki9689@users.noreply.github.com>
2024-04-30 19:09:04 +0000

    added solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [count,setCount]=useState(0)
+  const inc=()=>{
+    if(count >0){
+      setCount(count+1)
+    }
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times.</p>
+        <button onClick={inc}>Click me</button>
     </div>
   )
 }
